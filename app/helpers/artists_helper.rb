55baf78e60f2b14742838612e9d20e8ef49874f0
Judy McGoldrick <jjeanius@verizon.net>
2019-02-17 21:19:59 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -6,7 +6,7 @@ module ArtistsHelper
 
   def artist_select(song)
     if song.artist.nil?
-      select_tag "song[artist_id]", options_from_collection_for_select(:artist_select, Artist.all, :id, :name)
+      select_tag "song[artist_id]", options_from_collection_for_select(:artist_name, Artist.all, :id, :name)
     else
       hidden_field_tag "song[artist_id]", song.artist_id
     end
